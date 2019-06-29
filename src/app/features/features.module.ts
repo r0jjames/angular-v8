c90fefeeefd383c6578cf4feba554581399daa3a
unknown <rojjamescarranza@gmail.com>
2019-06-29 20:17:44 +0800

    remove unused import

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -1,6 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
 import { FeaturesRoutingModule } from './features-routing.module';
 import { SharedModule } from '../shared/shared.module';
 
