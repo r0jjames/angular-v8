5b3ca47c74fd73abbea8a1e107e206c7184e41a8
Roj JC Carranza <roj_jc_carranza@mfcgd.com>
2019-08-15 17:48:54 +0800

    Update core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,5 +1,5 @@
 import { EnsureModuleLoadedOnceGuard } from './ensure-module-loaded-once.guard';
-import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { NgModule, Optional, SkipSelf, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
@@ -12,4 +12,10 @@ export class CoreModule extends EnsureModuleLoadedOnceGuard {
     constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
       super(parentModule);
     }
+    static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: CoreModule,
+      providers: [ /** Put your core service here */ ]
+    };
+   }
  }
